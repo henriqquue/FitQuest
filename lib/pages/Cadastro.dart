ad1867556fa639376cb0b42206bd793958382097
Henrique Rezende <henrique.rezende667@gmail.com>
2023-10-09 08:55:10 -0300

    Mudança da tela de cadastro para stateful widget

--- lib/pages/Cadastro.dart
+++ lib/pages/Cadastro.dart
@@ -1,127 +1,139 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Cadastro extends StatelessWidget {
+
+
+class Cadastro extends StatefulWidget {
+  const Cadastro({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF0B235C),
-        body: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            SvgPicture.asset('assets/images/logo.svg', height: 350),
-            Center(
-              child: Text(
-                "Olá!",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            SizedBox(height: 50),
-            TextFormField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Nome completo:",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                    width: 2.5,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Email:",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                    width: 2.5,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Senha:",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                    width: 2.5,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Confirmar senha:",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                    width: 2.5,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 32),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Cadastrar'),
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  padding: EdgeInsets.all(20),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  State<Cadastro> createState() => _Cadastro();
 }
+
+class _Cadastro extends State<Cadastro> {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Color(0xFF0B235C),
+          body: ListView(
+            padding: EdgeInsets.all(16.0),
+            children: <Widget>[
+              SvgPicture.asset('assets/images/logo.svg', height: 350),
+              Center(
+                child: Text(
+                  "Olá!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Nome completo:",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                      width: 2.5,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Email:",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                      width: 2.5,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Senha:",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                      width: 2.5,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Confirmar senha:",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                      width: 2.5,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Cadastrar'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    padding: EdgeInsets.all(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
